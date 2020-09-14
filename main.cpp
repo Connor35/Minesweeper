abc3fd9b8d1d31ca26647483645939a884f8921c
Connor <whitbey35@gmail.com>
2020-09-13 17:26:59 -0700

    fixed error handling for user coordinates

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 void welcome();
 string color_display_on();
+bool valid_int(string);
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -77,10 +78,19 @@ int main(){
 
         // these coordinates are flipped so that the board works visually for user
         // get X and Y coordinates
-        cout << "Enter X coordinate: ";
-        int y = stoi(turn.get_user_input());
-        cout << "Enter Y coordinate: ";
-        int x = stoi(turn.get_user_input());
+        string x_str = "";
+        string y_str = "";
+        while (!(valid_int(y_str))){
+            cout << "Enter X coordinate: ";
+            y_str = turn.get_user_input();
+        }
+        int y = stoi(y_str);
+
+        while (!(valid_int(x_str))){
+            cout << "Enter Y coordinate: ";
+            x_str = turn.get_user_input();
+        }
+        int x = stoi(x_str);
 
         // execute their move
         if (mode == "f"){
@@ -151,5 +161,12 @@ string color_display_on(){
     }
     cout << endl;
     return user_input;
+}
+
+bool valid_int(string str){
+    if (str == "0" or str == "1" or str == "2" or str == "3" or str == "4" or str == "5" or str == "6" or str == "7" or str == "8" or str == "9" or str == "10" or str == "11" or str == "12" or str == "13" or str == "14" or str == "15" or str == "16" or str == "17" or str == "18" or str == "19" or str == "20" or str == "21" or str == "22" or str == "23" or str == "24") {
+        return true;
+    }
+    return false;
 }
 
